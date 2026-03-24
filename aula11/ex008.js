4fa0d9893ccb1e025952c587b869a0a61b24388d
Gabriel Vitor <gtvitor07@gmail.com>
2026-03-24 00:00:02 -0300

    Condições

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassaou a velocidade permitida, MULTADO!')
+}
+console.log('Dirija sempre com muita segurança')

71948a48d3fd9b0d451c3f9cbd198918be78254d
Gabriel Vitor <gtvitor07@gmail.com>
2026-03-24 23:51:08 -0300

    else if aninhada

---
+++ aula12/ex012.js
@@ -0,0 +1,13 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas`)
+
+if (hora < 12) {
+    console.log(`Bom Dia!`)
+} else if (hora < 18) {
+    console.log(`Boa Tarde!`)
+} else if (hora < 24) {
+    console.log(`Boa Noite!`)
+} else {
+    console.log(`Você digitou a hora de forma errada`)
+}

cea036d1f0a0fc0d1b1ccd2c8295b6ee21b5ab53
Gabriel Vitor <gtvitor07@gmail.com>
2026-03-26 23:31:48 -0300

    Hora do dia em js

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,23 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    
+
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#AFC0C9'
+    } else if (hora >= 12 && hora < 18) {
+        //BOA TARDE
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#724506'
+    } else {
+        //BOA NOITE
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#071738'
+    }
+}

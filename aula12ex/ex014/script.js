201cca0983afcd8bdd05c7323e08062f39115854
Gabriel Vitor <gtvitor07@gmail.com>
2026-03-26 23:35:43 -0300

    Add educa

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,6 +1,7 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
+    var sad = window.document.getElementById('educ')
     var data = new Date()
     var hora = data.getHours()
     
@@ -11,13 +12,16 @@ function carregar() {
         //BOM DIA
         img.src = 'imagens/manha.png'
         document.body.style.background = '#AFC0C9'
+        sad.innerHTML = 'Bom dia!!'
     } else if (hora >= 12 && hora < 18) {
         //BOA TARDE
         img.src = 'imagens/tarde.png'
         document.body.style.background = '#724506'
+        sad.innerHTML = 'Boa tarde!!'
     } else {
         //BOA NOITE
         img.src = 'imagens/noite.png'
         document.body.style.background = '#071738'
+        sad.innerHTML = 'Boa noite!!'
     }
 }
